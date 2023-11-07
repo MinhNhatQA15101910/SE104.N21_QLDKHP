e84b29af81effb7992e616707896d3bd65b39323
Do Nhat <dmnhat.pt@gmail.com>
2023-11-07 12:00:15 +0700

    Done test CT_PhieuDKHPDAL

--- DAL/IServices/ICT_PhieuDKHPDALService.cs
+++ DAL/IServices/ICT_PhieuDKHPDALService.cs
@@ -5,8 +5,8 @@ namespace DAL.IServices
 {
     public interface ICT_PhieuDKHPDALService
     {
-        void TaoCT_PhieuDKHP(CT_PhieuDKHP ct_PhieuDKHP);
-        void XoaDSMHDKHP(int maPhieu);
+        int TaoCT_PhieuDKHP(CT_PhieuDKHP ct_PhieuDKHP);
+        int XoaDSMHDKHP(int maPhieu);
         List<CT_PhieuDKHP> GetCT_PhieuDKHPs();
     }
 }
