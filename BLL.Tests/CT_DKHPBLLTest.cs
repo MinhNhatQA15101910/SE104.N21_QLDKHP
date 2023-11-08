63709fa607b2da6a840ec1c3d53b6b7386cf25fa
JustVvu <nguyenvanvu065@gmail.com>
2023-11-08 19:35:36 +0700

    Add test BLL for ChuongTrinhHoc, CT_DKHP, DoiTuong, DanhSachMonHocMo

---
+++ BLL.Tests/CT_DKHPBLLTest.cs
@@ -0,0 +1,49 @@
+﻿namespace BLL.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public class CT_DKHPBLLTest
+    {
+        #region Service
+        private readonly Mock<ICT_PhieuDKHPDALService> CT_PhieuDKHPDALServiceMock;
+        private readonly ICT_PhieuDKHPBLLService _CT_PhieuDKHPBLLService;
+        #endregion
+
+        #region Constructor
+        public CT_DKHPBLLTest()
+        {
+            CT_PhieuDKHPDALServiceMock = new Mock<ICT_PhieuDKHPDALService>();
+            _CT_PhieuDKHPBLLService = new CT_DKHPBLLService(CT_PhieuDKHPDALServiceMock.Object);
+        }
+        #endregion
+
+        #region TaoCT_PhieuDKHP
+        [Fact]
+        public void TaoCT_PhieuDKHP_VerifyExecuteDAL()
+        {
+            // Arrange
+            var ct_phieuDKHP = new CT_PhieuDKHP();
+
+            // Act
+            _CT_PhieuDKHPBLLService.TaoCT_PhieuDKHP(ct_phieuDKHP);
+
+            // Assert
+            CT_PhieuDKHPDALServiceMock.Verify(_ => _.TaoCT_PhieuDKHP(ct_phieuDKHP), Times.Once());
+        }
+        #endregion
+
+        #region XoaDSMHDKHP
+        [Fact]
+        public void XoaDSMHDKHPT_VerifyExecuteDAL()
+        {
+            // Arrange
+            const int maPhieu = 1;
+
+            // Act
+            _CT_PhieuDKHPBLLService.XoaDSMHDKHP(maPhieu);
+
+            // Assert
+            CT_PhieuDKHPDALServiceMock.Verify(_ => _.XoaDSMHDKHP(maPhieu), Times.Once());
+        }
+        #endregion
+    }
+}
---
+++ BLL.Tests/ChuongTrinhHocBLLTest.cs
@@ -0,0 +1,85 @@
+﻿namespace BLL.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public class ChuongTrinhHocBLLTest
+    {
+        #region Service
+        private readonly Mock<IChuongTrinhHocDALService> _chuongTrinhHocDALServiceMock;
+        private readonly IChuongTrinhHocBLLService _chuongTrinhHocBLLService;
+        #endregion
+
+        #region Constructor
+        public ChuongTrinhHocBLLTest()
+        {
+            _chuongTrinhHocDALServiceMock = new Mock<IChuongTrinhHocDALService>();
+            _chuongTrinhHocBLLService = new ChuongTrinhHocBLLService(_chuongTrinhHocDALServiceMock.Object);
+        }
+        #endregion
+
+        #region DeleteListCTHoc
+        [Fact]
+        public void DeleteListCTHoc_VerifyExecuteDAL()
+        {
+            // Arrange
+            string maNganh = "CNTT";
+            int hocKy = 1;
+
+            // Act
+            _chuongTrinhHocBLLService.DeleteListCTHoc(maNganh, hocKy);
+
+            // Assert
+            _chuongTrinhHocDALServiceMock.Verify(m => m.DeleteListCTHoc(maNganh, hocKy), Times.Once);
+        }
+        #endregion
+
+        #region GetAllCTHoc
+        [Fact]
+        public void GetAllCTHoc_VerifyExecuteDAL()
+        {
+            //Act
+            _chuongTrinhHocBLLService.GetAllCTHoc();
+
+            // Assert
+            _chuongTrinhHocDALServiceMock.Verify(_ => _.GetAllCTHoc(), Times.Once);
+        }
+        #endregion
+
+        #region AddCTHoc
+        [Fact]
+        public void AddCTHoc_VerifyExecuteDAL()
+        {
+            // Arrange
+            var chuongTrinhHoc = new ChuongTrinhHoc()
+            {
+                MaMH = "IE005",
+                MaNganh = "CNTT",
+                HocKy = 1,
+            };
+
+            // Act
+            _chuongTrinhHocBLLService.AddCTHoc(chuongTrinhHoc);
+
+            // Assert
+            _chuongTrinhHocDALServiceMock.Verify(m => m.AddCTHoc(chuongTrinhHoc), Times.Once);
+        }
+        #endregion
+
+        #region DeleteCTHoc
+        [Fact]
+        public void DeleteCTHoc_VerifyExecuteDAL()
+        {
+            // Arrange
+            string maMH = "IE005";
+            string maNganh = "CNTT";
+            int hocKy = 1;
+
+            // Act
+            _chuongTrinhHocBLLService.DeleteCTHoc(maMH, maNganh, hocKy);
+
+            // Assert
+            _chuongTrinhHocDALServiceMock.Verify(m => m.DeleteCTHoc(maMH, maNganh, hocKy), Times.Once);
+        }
+        #endregion
+
+    }
+}
